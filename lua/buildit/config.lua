795b288058e675f78da91b985fd162e27e593afb
David Cook <slocook@proton.me>
2026-02-04 18:32:19 -0600

    Update ninja config

--- lua/buildit/config.lua
+++ lua/buildit/config.lua
@@ -5,12 +5,17 @@ M.options = {
         build_dir = 'build',
         build_type = 'Debug',
         threads = 4
+    },
+    ninja = {
+        build_dir = 'build',
+        threads = 4
     }
 }
 
 function M.setup(user_opts)
     user_opts = user_opts or {}
     M.options.cmake = vim.tbl_deep_extend('force', M.options.cmake, user_opts.cmake or {})
+    M.options.ninja = vim.tbl_deep_extend('force', M.options.ninja, user_opts.ninja or {})
 end
 
 return M
--- lua/buildit/systems/ninja.lua
+++ lua/buildit/systems/ninja.lua
@@ -1,18 +1,22 @@
 local M = {}
 
 local output = require('buildit.output')
-local build_dir = 'build'
+local config = require('buildit.config').options.ninja
 
 function M.configure()
-    -- no-op
+    -- no-op: ninja doesn't have a configure step
 end
 
 function M.build()
-    output.run_cmd('ninja -C ' .. build_dir)
+    output.run_cmd('ninja -C ' .. config.build_dir .. ' -j ' .. config.threads)
 end
 
 function M.clean()
-    output.run_cmd('ninja -C ' .. build_dir .. ' clean')
+    output.run_cmd('ninja -C ' .. config.build_dir .. ' clean')
+end
+
+function M.test()
+    output.run_cmd('ninja -C ' .. config.build_dir .. ' test')
 end
 
 return M
