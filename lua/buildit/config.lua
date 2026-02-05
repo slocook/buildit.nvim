2955c0a72efbdd4e658934475c6f3150317d9035
David Cook <slocook@proton.me>
2026-02-04 18:33:23 -0600

    Update autotools config

--- lua/buildit/config.lua
+++ lua/buildit/config.lua
@@ -9,6 +9,9 @@ M.options = {
     ninja = {
         build_dir = 'build',
         threads = 4
+    },
+    autotools = {
+        threads = 4
     }
 }
 
@@ -16,6 +19,7 @@ function M.setup(user_opts)
     user_opts = user_opts or {}
     M.options.cmake = vim.tbl_deep_extend('force', M.options.cmake, user_opts.cmake or {})
     M.options.ninja = vim.tbl_deep_extend('force', M.options.ninja, user_opts.ninja or {})
+    M.options.autotools = vim.tbl_deep_extend('force', M.options.autotools, user_opts.autotools or {})
 end
 
 return M
--- lua/buildit/systems/autotools.lua
+++ lua/buildit/systems/autotools.lua
@@ -1,17 +1,22 @@
 local M = {}
 
 local output = require('buildit.output')
+local config = require('buildit.config').options.autotools
 
 function M.configure()
     output.run_cmd('./configure')
 end
 
 function M.build()
-    output.run_cmd('make')
+    output.run_cmd('make -j ' .. config.threads)
 end
 
 function M.clean()
     output.run_cmd('make clean')
+end
+
+function M.test()
+    output.run_cmd('make check')
 end
 
 return M
